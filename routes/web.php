3c812c0f077273dfa5881cd03afce553d39a4f4b
shige <kenta.shigemasa1999@gmail.com>
2022-06-10 18:45:18 +0900

    Q

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
+Auth::routes();
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('XXX','AAAController@bbb');
 Route::get('admin/profile/create','Admin\ProfileController@add');
 Route::get('admin/profile/edit','Admin\ProfileController@edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
